f5c9042ccf870294690fba2adc25a33d01a3c5c6
Hendrik Ranocha <mail@ranocha.de>
2020-09-01 07:52:14 +0200

    disable comparison of hashes in tests

--- test/test_2d.jl
+++ test/test_2d.jl
@@ -14,16 +14,22 @@ isdir(outdir) && rm(outdir, recursive=true)
   run_trixi("parameters.toml", n_steps_max=1)
 
   @testset "uniform mesh as PNG" begin
-    test_trixi2img_convert("solution_000000.h5", outdir,
-        hashes=[("solution_000000_scalar.png", "f7da610fc8ab16aa691b1b0ecd956d67d6f68e06")])
+    test_trixi2img_convert("solution_000000.h5", outdir)
+        # To be able to compare hashes, we would need to make sure that exactly the same versions
+        # of all required libraries are installed everywhere, which does not seem to be a good option
+        # right now.
+        # hashes=[("solution_000000_scalar.png", "f7da610fc8ab16aa691b1b0ecd956d67d6f68e06")])
   end
 
   # PDF tests do not work on Windows
   if !Sys.iswindows()
     @testset "uniform mesh as PDF with grid lines" begin
       test_trixi2img_convert("solution_000000.h5", outdir,
-          hashes=[("solution_000000_scalar.pdf", "e62024c18ae010748f8c7b840bf7475ae398799a")],
           format=:pdf, grid_lines=true)
+          # To be able to compare hashes, we would need to make sure that exactly the same versions
+          # of all required libraries are installed everywhere, which does not seem to be a good option
+          # right now.
+          # hashes=[("solution_000000_scalar.pdf", "e62024c18ae010748f8c7b840bf7475ae398799a")])
     end
   end
 end
